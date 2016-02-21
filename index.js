0bef61aa30f1f4feb91bd321f44e4d1757b9a44f
Alicia Frisone <a.frisone54@gmail.com>
2016-02-21 00:05:49 -0700

    good commit message 2

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ $(document).ready(function(){
 	var totalCorrect = 0;
 	var totalIncorrect = 0;
 	var currentLevel = "M"
+	var easyIndex = 0;
+	var mediumIndex = 0;
+	var hardIndex = 0;
+
 	//Pull data from JSON 
 	var questions = getJSONInput();
 	
 	//Set the current question
-	var currentQuestion = new Question(questions.QuestionBank[0].Question, questions.QuestionBank[0].Answers.CorrectAnswer, 
-		questions.QuestionBank[0].Answers.Incorrect1, questions.QuestionBank[0].Answers.Incorrect2, questions.QuestionBank[0].Answers.Incorrect3);
+	var currentQuestion = new Question(questions.QuestionBank.MediumBank[0].Question, questions.QuestionBank.MediumBank[0].Answers.CorrectAnswer, 
+		questions.QuestionBank.MediumBank[0].Answers.Incorrect1, questions.QuestionBank.MediumBank[0].Answers.Incorrect2, questions.QuestionBank.MediumBank[0].Answers.Incorrect3);
 	var CorrectAnswer = currentQuestion.answer1;
 
 	function newQuestionText(currQuestion){
@@ -44,12 +48,14 @@ $(document).ready(function(){
 	}
 	newQuestionText(currentQuestion);
 
+	//when submit is clicked
 	$("#me").click(function(){
 		var myForm = document.getElementById("myForm");
 		var selectedRadio = myForm["question"]
-		//console.log(selectedRadio.value);
 		var selectedAnswer;
-		//console.log(document.getElementById("thisnode2").textContent)
+		//increment question index to pull appropriate q from bank
+		incrementIndex();
+
 		switch(Number(selectedRadio.value)){
 			case 1: selectedAnswer = document.getElementById("thisnode2").textContent;
 				break;
@@ -73,9 +79,52 @@ $(document).ready(function(){
 			if(totalIncorrect >= 3){
 				downDifficulty();
 			}
-			console.log("Total incorrect: " + totalIncorrect);
+			console.log("Total incorrect: " + totalIncorrect+ " Current Diff: " + currentLevel);
 		}
+
+		updateQuestion();
 	});
+	
+	function updateQuestion(){
+		switch(currentLevel){
+			case "E": 
+				currentQuestion = new Question(questions.QuestionBank.EasyBank[easyIndex].Question, questions.QuestionBank.EasyBank[easyIndex].Answers.CorrectAnswer,
+					questions.QuestionBank.EasyBank[easyIndex].Answers.Incorrect1,questions.QuestionBank.EasyBank[easyIndex].Answers.Incorrect2, questions.QuestionBank.EasyBank[easyIndex].Answers.Incorrect3)
+				break;
+			case "M": 
+				currentQuestion = new Question(questions.QuestionBank.MediumBank[mediumIndex].Question, questions.QuestionBank.MediumBank[mediumIndex].Answers.CorrectAnswer,
+					questions.QuestionBank.MediumBank[mediumIndex].Answers.Incorrect1,questions.QuestionBank.MediumBank[mediumIndex].Answers.Incorrect2, questions.QuestionBank.MediumBank[mediumIndex].Answers.Incorrect3)
+				break;
+			case "H": 
+				currentQuestion = new Question(questions.QuestionBank.HardBank[hardIndex].Question, questions.QuestionBank.HardBank[hardIndex].Answers.CorrectAnswer,
+					questions.QuestionBank.HardBank[hardIndex].Answers.Incorrect1,questions.QuestionBank.HardBank[hardIndex].Answers.Incorrect2, questions.QuestionBank.HardBank[hardIndex].Answers.Incorrect3)
+				break;
+		}
+		newQuestionText(currentQuestion);
+	}
+
+	function incrementIndex(){
+		switch(currentLevel){
+			case "E": 
+				if(easyIndex < 2)
+					easyIndex += 1;
+				else
+					easyIndex = 0;
+				break;
+			case "M": 
+				if(mediumIndex < 2)
+					mediumIndex += 1;
+				else
+					mediumIndex = 0;
+				break;
+			case "H": 
+				if(hardIndex < 2)
+					hardIndex += 1;
+				else
+					hardIndex = 0;
+				break;
+		}
+	}
 
 	function upDifficulty(){
 		totalCorrect = 0;
@@ -117,46 +166,121 @@ $(document).ready(function(){
 	}
 
 	function getJSONInput(){
-		var temp = {"QuestionBank":[
+		var temp = {
+		"QuestionBank":{
 
-	    {
-	    "QuestionID":"000", 
-	    "Difficulty":"Medium", 
-	    "Question":"In Python, what is the command to display hello world to the standard output?",
-	    "Answers":{
-		     "CorrectAnswer": "print \"hello world\"", 
-		     "Incorrect1": "console.log \"hello world\"", 
-		     "Incorrect2": "cout << \"hello world\"", 
-		     "Incorrect3" : "disp (\"hello world\")" 
-		 }
-	     },
+			"EasyBank":[
 
-	    {
-	    "QuestionID":"001", 
-	    "Difficulty":"Easy", 
-	    "Question":"How do I store the value 1 to the variable x in python?",
-	    "Answers":{
-		     "CorrectAnswer": "x = 1", 
-		     "Incorrect1": "x == 1", 
-		     "Incorrect2": "int x == 1", 
-		     "Incorrect3" : " 1 = x " 
-		 }
-		 },
+			    {
+			    "QuestionID":"0000", 
+			    "Question":"How do I store the value 1 to the variable x in python?",
+			    "Answers":{
+				    "CorrectAnswer": "x = 1", 
+				    "Incorrect1": "x == 1", 
+				    "Incorrect2": "int x == 1", 
+				    "Incorrect3" : " 1 = x " 
+				}
+				},
 
-		 {
-	    "QuestionID":"002", 
-	    "Difficulty":"Hard", 
-	    "Question":"which of the following will display the value of x and a descriptive string to the standard output if x is a string?",
-	    "Answers":{
-		     "CorrectAnswer": "print \"The value of x is: {}\".format(x) ", 
-		     "Incorrect1": "print  \"The value of x is:\" + x ", 
-		     "Incorrect2": "y = \"The value of x is: {}\".format(x)", 
-		     "Incorrect3" : " y =  print  \"The value of x is:\" + xs" 
-		 }
-		 },
-		]}
+				{
+			    "QuestionID":"0001", 
+			    "Question":"YOU know....?",
+			    "Answers":{
+				    "CorrectAnswer": "Frodo", 
+				    "Incorrect1": "Odyseus", 
+				    "Incorrect2": " motion ", 
+				    "Incorrect3" : " c++ " 
+				}
+			    },
 
-	return temp;
+			    {
+			    "QuestionID":"0002", 
+			    "Question":"Easy question of all questions?",
+			    "Answers":{
+				    "CorrectAnswer": "BOOM!", 
+				    "Incorrect1": "b explode bomb", 
+				    "Incorrect2": " Pick ME! ", 
+				    "Incorrect3" : " Sonny " 
+				}
+			    }
+			 	
+			],
+
+			"MediumBank":[
+
+				{
+			    "QuestionID":"1000", 
+			    "Question":"In Python, what is the command to display hello world to the standard output?",
+			    "Answers":{
+				    "CorrectAnswer": "print \"hello world\"", 
+				    "Incorrect1": "console.log \"hello world\"", 
+				    "Incorrect2": "cout << \"hello world\"", 
+				    "Incorrect3" : "disp (\"hello world\")" 
+				}
+			    },
+
+			    {
+			    "QuestionID":"1001", 
+			    "Question":"Which of the folowing returns 0?",
+			    "Answers":{
+				     "CorrectAnswer": "0 if true else 1", 
+				     "Incorrect1": "1 if true else 0", 
+				     "Incorrect2": " x = 0 ", 
+				     "Incorrect3" : " if 0: 1 " 
+				 }
+			     },
+
+			    {
+			    "QuestionID":"1002", 
+			    "Question":"Medium question of all questions?",
+			    "Answers":{
+				    "CorrectAnswer": "42", 
+				    "Incorrect1": "i can haz cheesburger", 
+				    "Incorrect2": " F = ma ", 
+				    "Incorrect3" : " Newtons 3rd law of motion " 
+				}
+			    }
+			],
+
+			"HardBank":[
+
+				{
+			    "QuestionID":"2000", 
+			    "Question":"which of the following will display the value of x and a descriptive string to the standard output if x is a string?",
+			    "Answers":{
+				     "CorrectAnswer": "print \"The value of x is: {}\".format(x) ", 
+				     "Incorrect1": "print  \"The value of x is:\" + x ", 
+				     "Incorrect2": "y = \"The value of x is: {}\".format(x)", 
+				     "Incorrect3" : " y =  print  \"The value of x is:\" + xs" 
+				 }
+				 },
+
+				{
+			    "QuestionID":"2001", 
+			    "Question":"Which of the following represents a conditional statement in python?",
+			    "Answers":{
+				     "CorrectAnswer": "if ( x == 3 )", 
+				     "Incorrect1": "x = 2", 
+				     "Incorrect2": "x == 2", 
+				     "Incorrect3" : "print \"hello world\"" 
+				}
+			    },
+
+			    {
+			    "QuestionID":"2002", 
+			    "Question":"Hard question of all questions?",
+			    "Answers":{
+				    "CorrectAnswer": "Cake!", 
+				    "Incorrect1": "Cher", 
+				    "Incorrect2": " PV = nRT ", 
+				    "Incorrect3" : " PA = LU factorization " 
+				}
+			    }
+			]
+		}
+		}
+
+		return temp;
 	}
 });
 	
