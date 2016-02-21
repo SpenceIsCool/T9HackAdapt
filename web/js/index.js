588ce355e57694a470e19a1c6b9923b96a9008ce
Alicia Frisone <a.frisone54@gmail.com>
2016-02-21 08:46:03 -0700

    Changed difficulty header names

--- web/js/index.js
+++ web/js/index.js
@@ -165,7 +165,7 @@ $(document).ready(function(){
 			case "E": 
 				totalCorrect = 0;
 				totalIncorrect = 0;
-				document.getElementById("med").innerText = "Medium";
+				document.getElementById("med").innerText = "Intermediate";
 				document.getElementById("easy").innerText = " ";
 				document.getElementById("correctIncorrect").innerHTML = " ";
 				currentLevel = "M";
@@ -175,7 +175,7 @@ $(document).ready(function(){
 				totalCorrect = 0;
 				totalIncorrect = 0;
 				document.getElementById("med").innerText = " "
-				document.getElementById("hard").innerText = "Hard"
+				document.getElementById("hard").innerText = "Proficient"
 				document.getElementById("correctIncorrect").innerHTML = " ";
 				currentLevel = "H";
 				// document.getElementById("difficultyImage").innerHTML = "<img src=\"./img/DifficultyHard.jpg\" alt=\"icon\" />";
@@ -200,7 +200,7 @@ $(document).ready(function(){
 				totalIncorrect = 0; 
 				currentLevel = "E";
 				document.getElementById("med").innerText = " "
-				document.getElementById("easy").innerText = "Easy"
+				document.getElementById("easy").innerText = "Beginner"
 				document.getElementById("correctIncorrect").innerHTML = " ";
 				// document.getElementById("difficultyImage").innerHTML = "<img src=\"./img/DifficultyEasy.jpg\" alt=\"icon\" />";
 				break;
@@ -209,7 +209,7 @@ $(document).ready(function(){
 				totalCorrect = 0;
 				totalIncorrect = 0;
 				document.getElementById("hard").innerText = " "
-				document.getElementById("med").innerText = "Easy"
+				document.getElementById("med").innerText = "Intermediate"
 				document.getElementById("correctIncorrect").innerHTML = " ";
 				// document.getElementById("difficultyImage").innerHTML = "<img src=\"./img/DifficultyMed.jpg\" alt=\"icon\" />";
 				break;
