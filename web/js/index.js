fa1eaae561f44c71351eef4c65d585107c89c5ef
Spencer Wilson <draggod007@rgnt2-94-49-dhcp.int.colorado.edu>
2016-02-21 01:56:58 -0700

    small changes

--- web/js/index.js
+++ web/js/index.js
@@ -166,28 +166,27 @@ $(document).ready(function(){
 	}
 
 	function getJSONInput(){
-		var temp = {
-		"QuestionBank":{
+		var temp = 	{"QuestionBank":{
 
 			"EasyBank":[
 
 			    {
 			    "QuestionID":"0000", 
-			    "Question":"How do I store the value 1 to the variable x in python?",
+			    "Question":" How do I store the value 1 to the variable x in python?",
 			    "Answers":{
-				    "CorrectAnswer": "x = 1", 
-				    "Incorrect1": "x == 1", 
-				    "Incorrect2": "int x == 1", 
+				    "CorrectAnswer": " x = 1", 
+				    "Incorrect1": " x == 1", 
+				    "Incorrect2": " int x == 1", 
 				    "Incorrect3" : " 1 = x " 
 				}
 				},
 
 				{
 			    "QuestionID":"0001", 
-			    "Question":"YOU know....?",
+			    "Question":" YOU know....?",
 			    "Answers":{
-				    "CorrectAnswer": "Frodo", 
-				    "Incorrect1": "Odyseus", 
+				    "CorrectAnswer": " Frodo", 
+				    "Incorrect1": " Odyseus", 
 				    "Incorrect2": " motion ", 
 				    "Incorrect3" : " c++ " 
 				}
@@ -195,36 +194,68 @@ $(document).ready(function(){
 
 			    {
 			    "QuestionID":"0002", 
-			    "Question":"Easy question of all questions?",
+			    "Question":" Easy question of all questions?",
 			    "Answers":{
-				    "CorrectAnswer": "BOOM!", 
-				    "Incorrect1": "b explode bomb", 
+				    "CorrectAnswer": " BOOM!", 
+				    "Incorrect1": " b explode bomb", 
 				    "Incorrect2": " Pick ME! ", 
 				    "Incorrect3" : " Sonny " 
 				}
 			    }
+
+			    {
+			    "QuestionID":"0003", 
+			    "Question":" Easy question 003?",
+			    "Answers":{
+				    "CorrectAnswer": " this thingy!", 
+				    "Incorrect1": " no", 
+				    "Incorrect2": " no ", 
+				    "Incorrect3" : " no " 
+				}
+			    }
 			 	
+			 	{
+			    "QuestionID":"0004", 
+			    "Question":" Easy question 004?",
+			    "Answers":{
+				    "CorrectAnswer": " this thingy!", 
+				    "Incorrect1": " no", 
+				    "Incorrect2": " no ", 
+				    "Incorrect3" : " no " 
+				}
+			    }
+
+			    {
+			    "QuestionID":"0005", 
+			    "Question":" Easy question 005?",
+			    "Answers":{
+				    "CorrectAnswer": " this thingy!", 
+				    "Incorrect1": " no", 
+				    "Incorrect2": " no ", 
+				    "Incorrect3" : " no " 
+				}
+			    }
 			],
 
 			"MediumBank":[
 
 				{
 			    "QuestionID":"1000", 
-			    "Question":"In Python, what is the command to display hello world to the standard output?",
+			    "Question":" In Python, what is the command to display hello world to the standard output?",
 			    "Answers":{
-				    "CorrectAnswer": "print \"hello world\"", 
-				    "Incorrect1": "console.log \"hello world\"", 
-				    "Incorrect2": "cout << \"hello world\"", 
-				    "Incorrect3" : "disp (\"hello world\")" 
+				    "CorrectAnswer": " print \"hello world\"", 
+				    "Incorrect1": " console.log \"hello world\"", 
+				    "Incorrect2": " cout << \"hello world\"", 
+				    "Incorrect3" : " disp (\"hello world\")" 
 				}
 			    },
 
 			    {
 			    "QuestionID":"1001", 
-			    "Question":"Which of the folowing returns 0?",
+			    "Question":" Which of the folowing returns 0?",
 			    "Answers":{
-				     "CorrectAnswer": "0 if true else 1", 
-				     "Incorrect1": "1 if true else 0", 
+				     "CorrectAnswer": " 0 if true else 1", 
+				     "Incorrect1": " 1 if true else 0", 
 				     "Incorrect2": " x = 0 ", 
 				     "Incorrect3" : " if 0: 1 " 
 				 }
@@ -232,12 +263,45 @@ $(document).ready(function(){
 
 			    {
 			    "QuestionID":"1002", 
-			    "Question":"Medium question of all questions?",
+			    "Question":" Medium question of all questions?",
 			    "Answers":{
-				    "CorrectAnswer": "42", 
-				    "Incorrect1": "i can haz cheesburger", 
+				    "CorrectAnswer": " 42", 
+				    "Incorrect1": " i can haz cheesburger", 
 				    "Incorrect2": " F = ma ", 
 				    "Incorrect3" : " Newtons 3rd law of motion " 
+				}
+			    }
+
+			    {
+			    "QuestionID":"1003", 
+			    "Question":" Medium question 003?",
+			    "Answers":{
+				    "CorrectAnswer": " this thingy!", 
+				    "Incorrect1": " no", 
+				    "Incorrect2": " no ", 
+				    "Incorrect3" : " no " 
+				}
+			    }
+
+			    {
+			    "QuestionID":"1004", 
+			    "Question":" Medium question 004?",
+			    "Answers":{
+				    "CorrectAnswer": " this thingy!", 
+				    "Incorrect1": " no", 
+				    "Incorrect2": " no ", 
+				    "Incorrect3" : " no " 
+				}
+			    }
+
+			    {
+			    "QuestionID":"1005", 
+			    "Question":" Medium question 005?",
+			    "Answers":{
+				    "CorrectAnswer": " this thingy!", 
+				    "Incorrect1": " no", 
+				    "Incorrect2": " no ", 
+				    "Incorrect3" : " no " 
 				}
 			    }
 			],
@@ -248,9 +312,9 @@ $(document).ready(function(){
 			    "QuestionID":"2000", 
 			    "Question":"which of the following will display the value of x and a descriptive string to the standard output if x is a string?",
 			    "Answers":{
-				     "CorrectAnswer": "print \"The value of x is: {}\".format(x) ", 
-				     "Incorrect1": "print  \"The value of x is:\" + x ", 
-				     "Incorrect2": "y = \"The value of x is: {}\".format(x)", 
+				     "CorrectAnswer": " print \"The value of x is: {}\".format(x) ", 
+				     "Incorrect1": " print  \"The value of x is:\" + x ", 
+				     "Incorrect2": " y = \"The value of x is: {}\".format(x)", 
 				     "Incorrect3" : " y =  print  \"The value of x is:\" + xs" 
 				 }
 				 },
@@ -259,10 +323,10 @@ $(document).ready(function(){
 			    "QuestionID":"2001", 
 			    "Question":"Which of the following represents a conditional statement in python?",
 			    "Answers":{
-				     "CorrectAnswer": "if ( x == 3 )", 
-				     "Incorrect1": "x = 2", 
-				     "Incorrect2": "x == 2", 
-				     "Incorrect3" : "print \"hello world\"" 
+				     "CorrectAnswer": " if ( x == 3 )", 
+				     "Incorrect1": " x = 2", 
+				     "Incorrect2": " x == 2", 
+				     "Incorrect3" : " print \"hello world\"" 
 				}
 			    },
 
@@ -270,16 +334,48 @@ $(document).ready(function(){
 			    "QuestionID":"2002", 
 			    "Question":"Hard question of all questions?",
 			    "Answers":{
-				    "CorrectAnswer": "Cake!", 
-				    "Incorrect1": "Cher", 
+				    "CorrectAnswer": " Cake!", 
+				    "Incorrect1": " Cher", 
 				    "Incorrect2": " PV = nRT ", 
 				    "Incorrect3" : " PA = LU factorization " 
 				}
 			    }
+
+			    {
+			    "QuestionID":"2003", 
+			    "Question":" Hard question 003?",
+			    "Answers":{
+				    "CorrectAnswer": " this thingy!", 
+				    "Incorrect1": " no", 
+				    "Incorrect2": " no ", 
+				    "Incorrect3" : " no " 
+				}
+			    }
+
+			    {
+			    "QuestionID":"2004", 
+			    "Question":" Hard question 004?",
+			    "Answers":{
+				    "CorrectAnswer": " this thingy!", 
+				    "Incorrect1": " no", 
+				    "Incorrect2": " no ", 
+				    "Incorrect3" : " no " 
+				}
+			    }
+
+			    {
+			    "QuestionID":"2005", 
+			    "Question":" Hard question 005?",
+			    "Answers":{
+				    "CorrectAnswer": " this thingy!", 
+				    "Incorrect1": " no", 
+				    "Incorrect2": " no ", 
+				    "Incorrect3" : " no " 
+				}
+			    }
 			]
 		}
-		}
-
+	}
 		return temp;
 	}
 });
